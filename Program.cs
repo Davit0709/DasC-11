6f2c8330f377532f6d99ca89312e8940415725a1
Davit <dav0709@icloud.com>
2023-03-22 19:01:47 +0400

    uxum

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+
+
+        int[] arr = new int[4];
+
+        Console.WriteLine("Случайный массив:");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = rnd.Next(100, 1000);
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
+
+
+        int evenCount = 0;
+
+        foreach (int num in arr)
+        {
+            if (num % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+
+
+        Console.WriteLine("Количество чётных чисел в массиве: " + evenCount);
+
+
+        Console.ReadKey();
+    }
+}
